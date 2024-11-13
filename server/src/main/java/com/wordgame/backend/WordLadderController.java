a4b23eb531b0f856cf2b3530ba03c2e104db6803
Ian McLeod <35231746+Homiegfresh@users.noreply.github.com>
2024-11-13 08:42:34 -0600

    WIP
    - Built some of the basic UI components for the word ladder game.

--- server/src/main/java/com/wordgame/backend/WordLadderController.java
+++ server/src/main/java/com/wordgame/backend/WordLadderController.java
@@ -25,7 +25,9 @@ public class WordLadderController {
     public ResponseEntity<String> RandomWordLadderGame(@RequestParam int difficulty) {
         try (Connection connection = Objects.requireNonNull(jdbcTemplate.getDataSource()).getConnection();
             CallableStatement callableStatement = connection.prepareCall("{call sp_GetRandomWordLadderGame(?)}")) {
-            callableStatement.setInt(1, difficulty);
+            //callableStatement.setInt(1, difficulty);
+            // TESTING
+            callableStatement.setInt(1, 4);
 
             // Execute the stored procedure
             ResultSet rs = callableStatement.executeQuery();
