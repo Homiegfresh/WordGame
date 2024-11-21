e676d5541d475c1e5ceaac80c079153bce6080ad
Ethan <Enl2222@jagmail.southalabama.edu>
2024-11-20 19:33:33 -0600

    Makes new game work

--- client/src/main/java/com/wordgame/WinScreenController.java
+++ client/src/main/java/com/wordgame/WinScreenController.java
@@ -1,9 +1,13 @@
 package com.wordgame;
 
+import com.wordgame.models.enums.GameType;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WinScreenController {
     @FXML
@@ -13,7 +17,10 @@ public class WinScreenController {
     }
 
     @FXML
-    public void NewGame(MouseEvent event) {
-
+    public void NewGame(MouseEvent event) throws IOException {
+        GameType gametype = GameType.WordLadder;
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        ViewHelpers.<GameIntakeController>Navigate(stage,"Gameintake.fxml",controller ->
+                controller.initialize(gametype));
     }
 }
